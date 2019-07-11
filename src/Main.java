3f3faf94832be2104836963ea427716cf8a9543b
Lirone29 <kalata.eliza@gmail.com>
2019-07-11 09:59:25 +0200

    DEMO - Communication with modem using console. Fixed Card SIM reading.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import gnu.io.CommPortIdentifier;
-
-import java.util.Enumeration;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         ModemComm modemComm = new ModemComm();
     }
--- src/ModemComm.java
+++ src/ModemComm.java
@@ -9,15 +9,29 @@ import java.util.TooManyListenersException;
 
 public class ModemComm {
 
-    String menu = "Choose action \n" +
+    public enum PINSecurity{
+        READY, //no password
+        PIN,
+        PUK,
+        PIN2,
+        PUK2,
+        NONE
+    }
+
+    String menu = "-----Choose action----- \n" +
             "1. Read information about modem \n" +
-            "2. Ask if PIN | PUCK requiered \n" +
-            "3. Write own commend \n " +
+            "2. Request IMSI \n"+
+            "3. Ask if PIN | PUCK required \n" +
+            "4. Write own commend \n" +
+            "5. Check available numbers \n"+
+            "6. Write number \n"+
+            "7. Clear number \n"+
             "0. EXIT \n";
 
     //Connection parameters?? Is it necessery to configure???
 
     int timeout = 2000;
+
     static Enumeration<CommPortIdentifier> portList;
     static CommPortIdentifier portId;
 
@@ -25,32 +39,45 @@ public class ModemComm {
     static OutputStream outputStream;
     static InputStream inputStream;
 
-    int boudRate;
+    static String answer = "";
+    static String IMSI = "";
+    PINSecurity pinSecurity = PINSecurity.NONE;
 
     //PORT NAME
     static String modemPort = "COM13";
 
-    static int phoneBookNumber = 1;
+    static String PIN = "";
+    static String Puck = "";
+    static int phoneBookUserNumber = 1;
+   // static int phoneBookNumber = 1;
     String phoneNumber = "+48000000000";
 
-    //Messages
+    //Messages:
+
+    static String CR = "\r\n";
     String ownMessage = "";
-    static String messageModemId = "ATI\r\n";
-    static String messageAskPIN = "AT+CPIN?\r\n";
+    static String messageModemId = "ATI" +CR;
+    static String messageAskPIN = "AT+CPIN?" + CR;
+
+    static String messageWritePIN = "AT+CPIN=" +CR;
+    //Identyfikacja karty SIM przy pomocy IMSI
+    static String messageIMSI = "AT+CIMI" + CR;
+
+    //"P2" SIM PIN2
+    //"SC" SIM card (if this parameter is set, MT will request the password during startup)
+    // change PIN - AT+CPWD="SC","9999","1234" || "AT+CPIN=\"0000\"";
+
     static String messageEnterPIN = "AT+CPIN=";
-    static String messageSelectOperator = "AT+COPS\r\n";
-    static String messageRegisterNetwork = "AT+CREG\r\n";
-    static String messageChangePassword = "AT+CPWD\r\n";
+    static String messageSelectOperator = "AT+COPS" + CR;
+    static String messageRegisterNetwork = "AT+CREG" + CR;
+    static String messageChangePassword = "AT+CPWD" + CR;
 
     //returns all numbers Currently written in PhoneBook| the MSISDN
-    static String messageSubscriberNumber = "AT+CNUM\r\n";
-    static String messageSignalQuality = "AT+CSQ\r\n";
-    static String messageSelectPhonebookMemoryStorage = "AT+CPBS=\"ON\"\r\n";
+    static String messageCheckNumbers = "AT+CNUM" + CR;
+    //DONE
+    static String messageSignalQuality = "AT+CSQ" +CR;
+    static String messageSelectPhonebookMemoryStorage = "AT+CPBS=\"ON\"" +CR;
     static String messageWritePhonebook = "AT+CPBW=";
-
-
-    static String messageString2 = "AT+CPIN=\"7078\"";
-    static String CR = "\r\n";
 
     //read all available ports
     public static void getAllPorts() {
@@ -80,7 +107,7 @@ public class ModemComm {
 
                     // Port owner and connection timeout
                     serialPort = (SerialPort) portId.open(modemPort, timeout);
-                    boudRate = serialPort.getBaudRate();
+                    //boudRate = serialPort.getBaudRate();
 
                     // Configure connection parameters - baudrate,  dataBits,   stopBits,   parity
                     serialPort.setSerialPortParams(57600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
@@ -119,17 +146,19 @@ public class ModemComm {
 
         public void serialEvent(SerialPortEvent arg0) {
             int data;
-            String answer = "";
+            String endAnswer = "";
             try {
+                answer = "";
                 int len = 0;
                 while ((data = in.read()) > -1) {
                     if (data == 'O') {
-                        answer = String.valueOf(data);
+                        endAnswer = String.valueOf(data);
                     }
-                    if (data == 'K' && answer.equals("O")) {
+                    if (data == 'K' && endAnswer.equals("O")) {
                         break;
                     }
                     buffer[len++] = (byte) data;
+                    answer += String.valueOf((byte) data);
                 }
                 System.out.print(new String(buffer, 0, len));
             } catch (IOException e) {
@@ -153,15 +182,15 @@ public class ModemComm {
             try {
                 int i = 0;
                 byte[] c = b;
+               // synchronized(this.out);
                 while ((i) < (c.length)) {
-                    this.out.write(c[i]);
-                    System.out.print(c[i]);
-                    i++;
+                        this.out.write(c[i]);
+                        //System.out.print(String.valueOf(c[i]));
+                        i++;
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.print("Thread ended");
             return;
         }
     }
@@ -185,7 +214,14 @@ public class ModemComm {
 
     public void writePhoneNumber(BufferedReader readerTmp) throws IOException {
         BufferedReader reader = readerTmp;
-        (new Thread(new SerialWriter(outputStream, messageModemId))).start();
+        Thread t1 = new Thread(new SerialWriter(outputStream, messageSelectPhonebookMemoryStorage));
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Write phone number!");
         phoneNumber = reader.readLine();
         //145 - whe number has +48/ other country number in iths cypher
@@ -193,8 +229,34 @@ public class ModemComm {
         int type = 145;
         System.out.println("Set text for user - alias/name");
         String text = String.valueOf(reader.readLine());
-        String mesWritePhonebook = messageWritePhonebook += phoneNumber + ",\"+48" + phoneNumber + "\"," + type + "\",\"" + text + "\"";
-        (new Thread(new SerialWriter(outputStream, messageWritePhonebook))).start();
+        phoneNumber = "+48" +phoneNumber;
+        String mesWritePhonebook = messageWritePhonebook += phoneBookUserNumber + ",\"" + phoneNumber + "\"," + type + ",\"" + text + "\"" +CR;
+
+        Thread t2 = (new Thread(new SerialWriter(outputStream, mesWritePhonebook)));
+        t2.start();
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void clearNumber() {
+
+        Thread t1 = new Thread(new SerialWriter(outputStream, messageSelectPhonebookMemoryStorage));
+        t1.start();
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread t2 = new Thread(new SerialWriter(outputStream, "AT+CPBW=" + phoneBookUserNumber + CR));
+        t2.start();
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -216,6 +278,7 @@ public class ModemComm {
             try {
                 while (readLine.equals("")) {
                     readLine = reader.readLine();
+                    if(readLine.equals(""))continue;
                     if (Integer.parseInt(readLine) < 0 || Integer.parseInt(readLine) > 10) {
                         System.out.println("Wrong number given! Try again.");
                         continue;
@@ -235,14 +298,35 @@ public class ModemComm {
                     tmp = reader.readLine();
                     break;
                 }
-                case 2: {
-                    (new Thread(new SerialWriter(outputStream, messageAskPIN))).start();
+                case 2:{
+                    (new Thread(new SerialWriter(outputStream, messageIMSI))).start();
                     tmp = reader.readLine();
                     break;
                 }
                 case 3: {
+                    (new Thread(new SerialWriter(outputStream, messageAskPIN))).start();
+                    tmp = reader.readLine();
+                    break;
+                }
+                case 4: {
+                    System.out.println("Write Command: \n");
                     ownMessage = reader.readLine() + CR;
                     (new Thread(new SerialWriter(outputStream, ownMessage))).start();
+                    tmp = reader.readLine();
+                    break;
+                }
+                case 5:{
+                    (new Thread(new SerialWriter(outputStream, messageCheckNumbers))).start();
+                    tmp = reader.readLine();
+                    break;
+                }
+                case 6:{
+                    writePhoneNumber(reader);
+                    tmp = reader.readLine();
+                    break;
+                }
+                case 7:{
+                    clearNumber();
                     tmp = reader.readLine();
                     break;
                 }
@@ -255,23 +339,8 @@ public class ModemComm {
 
     }
 
-    public void writeCommand(String command) {
-        byte[] b = messageModemId.getBytes(StandardCharsets.UTF_8);
-        try {
-            int i = 0;
-            byte[] c = b;
-            while ((i) < (c.length)) {
-                outputStream.write(c[i]);
-                System.out.println(c[i]);
-                i++;
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public ModemComm() {
+
         getAllPorts();
         setModemPort();
         boolean connection = connect();
